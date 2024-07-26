78cd498041009588ae06c8de99920d8d10aa6e64
Victor Oladeji Betiku <140535561+viskky@users.noreply.github.com>
2024-07-26 17:26:23 +0100

    human_genome_variant_calling.bash

--- scripts/human_genome_variant_calling.bash
+++ scripts/human_genome_variant_calling.bash
@@ -44,14 +44,3 @@ bcftools mpileup -o b -o raw.bcf -f hg38.fa --threads 8 -q 20 -Q 30 output.sorte
 #variant calling
 bcftools call -m -v -o variants.raw.vcf raw.bcf
 
-#Count number of SNPs
-bcftools view -v snps variants.raw.vcf | grep -v -c '^#' variants.raw.vcf
-
-#Count number of INDELs
-bcftools view -v indels variants.raw.vcf | grep -v -c '^#' variants.raw.vcf
-
-#Filter variants using qualitycriteria >=30
-bcftools filter -i 'QUAL>=30' variants.raw.vcf | grep -v -c '^#' 
-
-
-bcftools filter -i 'QUAL>=30' variants.raw.vcf | sort | head -n 100
